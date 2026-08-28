df763404f56c1b440aa60f053c7b22743cc18b02
CsengeriEvelin <evelin.csengeri@gmail.com>
2026-08-28 18:51:36 +0200

    MySql package is added

--- UserAdmin/obj/Debug/net10.0-windows/UserAdmin.AssemblyInfo.cs
+++ UserAdmin/obj/Debug/net10.0-windows/UserAdmin.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UserAdmin")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b0b2645af2e894ec528b39c0e4f4e47165fc449d")]
 [assembly: System.Reflection.AssemblyProductAttribute("UserAdmin")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UserAdmin")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
